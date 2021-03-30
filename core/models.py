554135a8b6e8e049f19b003b525b3dc355dcc1a5
InvalidSB <invalidSB45@gmail.com>
2021-03-30 21:38:56 +0545

    Blog page edited

--- core/models.py
+++ core/models.py
@@ -30,11 +30,7 @@ class SelectedWorks(models.Model):
 
 
 class blogHaru(models.Model):
-	# title=models.CharField(max_length=50,verbose_name="my blogs" )
-	# description=models.TextField(max_length=1000)
-	# date_posted = models.DateTimeField(default=timezone.now)
-	# def __str__(self):
-	# 	return self.title
+	
 	blog_category = [
 		('Programming', 'Programming'),
 		('Travel', 'Travel'),
@@ -46,7 +42,7 @@ class blogHaru(models.Model):
 	title = models.CharField(max_length=200)
 	# subtitle = models.CharField(max_length=200)
 	description=models.TextField()
-	thumbnail = models.ImageField(upload_to='blog_thumbnails')
+	image = models.ImageField(upload_to='Blog_images')
 	date_posted = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
